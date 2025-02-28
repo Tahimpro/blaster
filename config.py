33acc2e1fa6024061187a8e4a1a30846d66a59e1
Tahim Mondal <133028260+Tahimpro@users.noreply.github.com>
2025-02-28 10:38:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 from telethon import TelegramClient
 
 # Ensure your API credentials are strings
-API_ID = os.environ.get("API_ID", "23990433")
-API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5811431199:AAGad6YjD5nC9PTgurWIbRM4krUpopevpf8")
+API_ID = os.environ.get("API_ID", "16531092")
+API_HASH = os.environ.get("API_HASH", "b073b97bd4c8c56616fc2cbbd4da845a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7843096547:AAHzkh6gwbeYzUrwQmNlskzft6ZayCRKgNU")
 
 assert isinstance(API_ID, str), "API_ID must be a string"
 assert isinstance(API_HASH, str), "API_HASH must be a string"
